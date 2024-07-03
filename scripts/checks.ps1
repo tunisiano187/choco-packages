e54276498147a7197faa33092584bf5d1f303cbf
Tunisiano187 <helpdesk.choc@gmail.com>
2024-07-03 17:41:52 +0200

    checks

---
+++ scripts/checks.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "Checking nuspec errors"
+scripts/Find-NuspecError.ps1
+git commit -am "Nuspec errors"
+Write-Host "updating packageSourceUrl"
+scripts/Update-PackageSourceUrl.ps1 -GithubRepository "tunisiano187/Chocolatey-packages" -UseStopwatch
+git commit -am "PackageSourceUrl"
+Write-Host "updating variables in ps1"
+scripts/Update-Variables.ps1
+git commit -am "ps1 vars"
+Write-Host "updating IconUrl in nuspec"
+scripts/Update-IconUrl.ps1 -Quiet -GithubRepository "tunisiano187/Chocolatey-packages" -UseStopwatch
+git commit -am "Updating icons"
+Write-Host "updating owners in nuspec"
+import-module Wormies-AU-Helpers
+$nuspec=Get-ChildItem ./*.nuspec -Recurse; foreach ($file in $nuspec) { Update-Metadata -key owners -value "tunisiano" -NuspecFile $file.FullName }
+git commit -am "Updating owner"
+Write-Host "Updating Packages.md"
+scripts/ListPackages.ps1
+git commit -am "List packages"
+    
