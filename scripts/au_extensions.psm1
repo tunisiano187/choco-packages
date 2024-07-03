aa5ed87da13818b93f666188f48dd3ee7510dc81
Tunisiano187 <helpdesk.choc@gmail.com>
2024-07-03 14:18:11 +0200

    functions

---
+++ scripts/au_extensions.psm1
@@ -0,0 +1,24 @@
+# Export all the extensions that are meant to be used
+# within a au update script here.
+
+# We just specify the functions we want to export
+# but the file containing the functions is expected
+# to be named using the same name.
+$funcs = @(
+  'Get-FileVersion'
+  'Set-DescriptionFromReadme'
+  'Update-ChangelogVersion'
+  'Update-OnETagChanged'
+  'Invoke-VirusTotalScan'
+  'Get-GithubRepositoryLicense'
+  'Get-UserAgent'
+)
+
+$funcs | % {
+  if (Test-Path "$PSScriptRoot\$_.ps1") {
+    . "$PSScriptRoot\$_.ps1"
+    if (Get-Command $_ -ea 0) {
+      Export-ModuleMember -Function $_
+    }
+  }
+}
