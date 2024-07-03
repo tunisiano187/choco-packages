f8189fc9d50cbcb299802465fe0c2c3f89fad183
Tunisiano187 <helpdesk.choc@gmail.com>
2024-07-03 17:23:01 +0200

    improvement scripts

---
+++ scripts/Find-NuspecError.ps1
@@ -0,0 +1,65 @@
+﻿<#
+.SYNOPSIS
+    Check the content of the nuspec for common errror and output them
+
+.DESCRIPTION
+    Check the nuspec for missing fields, incorrect characters, ... and output them
+
+.PARAMETER folder
+    The folder where the packages are
+
+.OUTPUTS
+    The errors or OK
+#>
+function Find-NuspecError {
+    param(
+        [string]$folder = '../automatic/'
+    )
+
+    $folder = "$PSScriptRoot/$($folder)"
+
+    ## Update to the latest official xml
+    #choco new test --outputdirectory $env:TEMP\Chocolatey\
+    #$nuspecofficel = Get-Content -Path "$env:TEMP\Chocolatey\test\test.nuspec"
+    #$nuspecstart = $nuspecofficel.Where({$_ -match "<metadata>"},'until') | Where-Object {$_ -notmatch "^\s*#"} | ForEach-Object {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | ForEach-Object {
+    #    if(($_ -notmatch '\*') -and (($_ -match '<package') -or ($_ -match 'Ω') -or ($_ -match '<?xml')))
+    #    {
+    #        $_
+    #    }
+    #}
+
+    $nuspecs = Get-ChildItem -Path $folder -Filter *.nuspec -Recurse
+
+    foreach ($nuspec in $nuspecs) {
+        [xml]$content = Get-Content $nuspec.FullName
+        $filecontent = Get-Content $nuspec.FullName
+
+#        # Get the latest official xml informations
+#        $nuspecend = $filecontent.Where({$_ -match "<metadata>"},'skipuntil') | Select-Object -Skip 1
+#        $filecontent = "$nuspecstart
+#$nuspecend"
+
+        # Check if the files section exists to avoid embedding useless files
+        If(!($filecontent -match '<files>')) {
+            $errormsg = ': missing <files><file src="tools\**" target="tools" /></files>'
+            (Get-Content $nuspec.FullName) -replace "</metadata>", '</metadata>
+  <files>
+    <file src="tools\**" target="tools" />
+  </files>' | Set-Content $nuspec.FullName
+#            throw "$($nuspec.Name): $($errormsg)"
+        }
+
+        # Check if the packageSourceURL section exists
+        If(!($filecontent -match 'packageSourceUrl')) {
+            throw "$($nuspec.Name): missing <packageSourceUrl></packageSourceUrl>"
+        }
+
+        # check if the description contains French words
+        If($content.package.metadata.description -match " est ") {
+            throw "$($nuspec.Name): Description in French should be in english"
+        }
+
+    }
+}
+
+Find-NuspecError
---
+++ scripts/ListPackages.ps1
@@ -0,0 +1,33 @@
+$ErrorActionPreference = 'Continue';
+#git checkout origin master
+$mdfile = "$PSScriptRoot/../Packages.md"
+Set-Content $mdfile "# Automatic Packages currently maintained here"
+Add-Content $mdfile "| Downloads | Package Name and version | Repology |"
+Add-Content $mdfile "|-----------:|--------------|--------------|"
+$paths = Get-ChildItem -Path "$PSScriptRoot/../automatic/" -Directory | Select-Object FullName
+foreach ($path in $paths) {
+    $package = $path.FullName.split('\')[-1]
+    Add-Content $mdfile "| [![$package](http://img.shields.io/chocolatey/dt/$package.svg)](https://chocolatey.org/packages/$package) | [![$package](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|[![Chocolatey package](https://repology.org/badge/latest-versions/$package.svg)](https://repology.org/project/$package/versions)|"
+}
+git add $mdfile
+git commit -m "[skip ci] List Packages"
+
+$mdfile = "$PSScriptRoot/../ToCorrect.md"
+Set-Content $mdfile "# To Correct"
+Add-Content $mdfile "| Package Name and version |"
+Add-Content $mdfile "|------------|"
+$paths = Get-ChildItem -Path "$PSScriptRoot/../automatic/" -Directory | Select-Object FullName
+foreach ($path in $paths) {
+    $package = $path.FullName.split('\')[-1]
+    if(Test-Path "$($path.FullName)\tools\") {
+        $ps1s = Get-ChildItem -Path "$($path.FullName)\tools\" -File -Filter ".ps1"
+        foreach ($ps1file in $ps1s) {
+            $content = Get-Content $ps1file | Select-Object {$_ -match 'headers.add'}
+            if($content.Lenght -gt 0) {
+                Add-Content $ps1file "|$package|"
+            }
+        }
+    }
+}
+git add $mdfile
+git commit -m "[skip ci] Deprecate custom WebClient code"
