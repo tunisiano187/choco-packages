58f5ae90cbbd15ac5fd7d815fa2efb380ac5a409
Tunisiano187 <helpdesk.choc@gmail.com>
2024-07-05 19:10:28 +0200

    $env:ChocolateyChecksumType

---
+++ au/requisites.ps1
@@ -0,0 +1,21 @@
+﻿# Define $env:ChocolateyChecksumType from chocolateyInstall file
+choco new test --outputdirectory $env:TEMP\Chocolatey\
+$fichier = "$env:TEMP\Chocolatey\test\tools\chocolateyInstall.ps1"
+
+# Read file content
+$contenu = Get-Content $fichier -Raw
+
+# Define the regex filter
+$pattern = "checksumType\s*=\s*'([^']*)'"
+
+# Find the checksumType
+if ($contenu -match $pattern) {
+    $checksumType = $Matches[1]
+    # Set to $env:ChocolateyChecksumType
+    $env:ChocolateyChecksumType = $checksumType
+} else {
+    Write-Output "Hash type not found set it to default."
+    $env:ChocolateyChecksumType = "SHA512"
+}
+
+Write-Output "Hash type is $env:ChocolateyChecksumType."
