c064448cf153b7a97d4a369af2b8f4098d15eaa7
Tunisiano187 <helpdesk.choc@gmail.com>
2024-07-10 11:52:10 +0200

    From chocolatey-packages

---
+++ automatic/hashcat/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = "$(Get-ToolsLocation)"
+
+$file       = (Get-ChildItem -Path $toolsDir -Filter "*.7z").FullName
+
+$packageArgs = @{
+    packageName     = $env:ChocolateyPackageName
+    unzipLocation   = $installDir
+
+    File            = $file
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$psfile     = Join-Path "$toolsDir" "hashcat.ps1"
+Install-ChocolateyPowershellCommand -PackageName "hashcat" -PSFileFullPath "$psfile"
---
+++ automatic/hashcat/tools/chocolateyuninstall.ps1
@@ -0,0 +1,1 @@
+﻿Remove-Item "$(Get-ToolsLocation)\hashcat*" -recurse
