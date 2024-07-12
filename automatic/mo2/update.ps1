a06a4e3be109839228e281ef2fbf663e88de7ea4
Tunisiano <helpdesk.choc@gmail.com>
2024-07-12 17:25:10 +0000

    ps1 vars

--- automatic/mo2/update.ps1
+++ automatic/mo2/update.ps1
@@ -1,4 +1,5 @@
-﻿import-module au
+import-module Chocolatey-AU
+Import-Module ..\..\scripts\au_extensions.psm1
 
 $releases = 'https://github.com/ModOrganizer2/modorganizer/releases/latest'
 $Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
