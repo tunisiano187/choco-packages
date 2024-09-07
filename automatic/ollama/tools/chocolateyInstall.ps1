b99b84869a3486a8700d9174281154a01ea15e72
Tunisiano187 <fabian@bowlman.org>
2024-09-07 19:13:47 +0200

    Ollama

---
+++ automatic/ollama/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$url            = 'https://github.com/ollama/ollama/releases/download/v0.3.9/OllamaSetup.exe'
+$checksum       = 'eecce97ef53eb577dbf3e2cbfae2055acae5ebfb20df22ce7285d9eed7adcce8'
+$checksumType   = 'sha256'
+
+$packageArgs = @{
+    packageName            = $env:ChocolateyPackageName
+    fileType               = 'EXE'
+    url                    = $url
+    checksum               = $checksum
+    checksumType           = $checksumType
+    silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    validExitCodes         = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
