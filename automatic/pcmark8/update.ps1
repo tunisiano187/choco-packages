f9e4f32cff0dfe5cc4aeaf3fbfbad56c7a40c10c
Tunisiano <helpdesk.choc@gmail.com>
2024-09-16 20:39:50 +0000

    ps1 vars

--- automatic/pcmark8/update.ps1
+++ automatic/pcmark8/update.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Stop'
-import-module au
+import-module Chocolatey-AU
+Import-Module ..\..\scripts\au_extensions.psm1
 
 $releases = "https://benchmarks.ul.com/downloads/pcmark8.zip"
 
