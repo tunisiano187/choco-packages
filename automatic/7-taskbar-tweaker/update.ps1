cbbec59ec8e4fe84cf4516378379d26fa7871432
Tunisiano187 <helpdesk.choc@gmail.com>
2025-04-03 18:09:08 +0200

    new updater

--- automatic/7-taskbar-tweaker/update.ps1
+++ automatic/7-taskbar-tweaker/update.ps1
@@ -23,6 +23,29 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
     $url32 = $releases
 
+	$url = "https://raw.githubusercontent.com/m417z/7-Taskbar-Tweaker/refs/heads/master/include/version.h"
+	try {
+		$content = Invoke-WebRequest -Uri $url -UseBasicParsing
+		$content = $content.Content
+	} catch {
+		Write-Error "Erreur lors du téléchargement du fichier : $($_.Exception.Message)"
+		exit
+	}
+
+	if ($content -match '#define VERSION_MAJOR\s+(\d+)') {
+		$versionMajor = $Matches[1]
+	}
+
+	if ($content -match '#define VERSION_MINOR\s+(\d+)') {
+    	$versionMinor = $Matches[1]
+	}
+
+	if ($content -match '#define VERSION_BUILD\s+(\d+)') {
+    	$versionBuild = $Matches[1]
+	}
+	$version = "$versionMajor.$versionMinor.$versionBuild"
+
+	
 	$File = "$($env:TEMP)\7tt_setup.exe"
 	Invoke-WebRequest -Uri $url32 -OutFile $File
     $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
