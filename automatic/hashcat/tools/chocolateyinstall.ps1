1230d9b4791086c8d5cd4e97f7b4b114062e9dc5
Tunisiano <helpdesk.choc@gmail.com>
2026-04-13 17:52:53 +0000

    ps1 vars

--- automatic/hashcat/tools/chocolateyinstall.ps1
+++ automatic/hashcat/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installDir = "$(Get-ToolsLocation)"
 
 $file       = (Get-ChildItem -Path $toolsDir -Filter "*.7z").FullName
@@ -15,7 +15,7 @@ Install-ChocolateyZipPackage @packageArgs
 
 $psfile     = Join-Path "$toolsDir" "hashcat.ps1"
 
-# resolve installation Path during installation where admin user has access the
+# resolve installation Path during installation Where-Object admin user has access the
 # $env:ChocolateyToolsLocation variable
 $hashcatDir = (Get-ChildItem "$env:ChocolateyToolsLocation\hashcat*" -Directory).FullName
 # Insert installation path into Powershell-Script
